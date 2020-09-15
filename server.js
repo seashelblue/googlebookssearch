7e450bce95de985de49c6dae71b0cfc5d1c64555
Shelsea <yilmazshelsea@gmail.com>
2020-09-15 09:50:27 -0500

    Almost done

---
+++ server.js
@@ -0,0 +1,31 @@
+//Techonology needed to begin and start app
+const express = require("express");
+const app = express();
+const routes = require("./routes")
+
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//For connecting MongoDB
+const mongoose = require("mongoose");
+// Connect to the Mongo DB
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://workout:workout1@ds021462.mlab.com:21462/heroku_7db557hn");
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+const PORT = process.env.PORT || 3001;
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
+
